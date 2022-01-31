a7a92bd3010aecca1cc42c477f7e654fc9d26748
Artur Słomowski <artur.slomowski@gmail.com>
2022-02-01 00:56:36 +0100

    remove chrome notification

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { Header } from './components/header/header';
 import { Notifications } from './components/notifications/Notifications';
 import { TodoApp } from './components/todoApp/todoApp';
-import { requestNotificationsPermission } from './services/notifications';
 
-requestNotificationsPermission();
 
 const App = () => <>
     <Header />
